4aeee3e5a90fa01b745186b697e9d62bea9c0700
hemaxiSodvadiya <111499619+hemaxiSodvadiya@users.noreply.github.com>
2022-11-19 18:39:45 +0545

    Update choose_plan_screen.dart

--- lib/screens/choose_plan_screen.dart
+++ lib/screens/choose_plan_screen.dart
@@ -18,127 +18,124 @@ class ChoosePlanScreen extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            color: const Color(0xffC19E82),
+            // color: const Color(0xffC19E82),
             padding: const EdgeInsets.all(15),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 CustomAppBar(),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                  child: Text(
-                    Strings.chooseAPlan,
-                    style: TextStyles.headingTextStyle,
+                SizedBox(height: 20),
+                Text(
+                  "Choose a plan",
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white.withOpacity(0.8),
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
+                const SizedBox(height: 15),
                 Expanded(
-                  child: FutureBuilder(
-                    future: DbHelper.dbHelper
-                        .fetchAllRespons(data: Global.wildAnimalSplashData),
-                    builder: (context, snapShot) {
-                      if (snapShot.hasData) {
-                        List<WildAnimal>? res = snapShot.data;
-
-                        print("+++++++");
-                        return ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: Global.wildAnimalSplashData.length,
-                            itemBuilder: (context, i) {
-                              return Container(
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 20, vertical: 5),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
+                    child: FutureBuilder(
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      List<DBData>? res = snapshot.data;
+                      return ListView.builder(
+                        itemBuilder: (context, i) {
+                          return Container(
+                            margin: EdgeInsets.all(10),
+                            height: 120,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(7),
+                              image: DecorationImage(
+                                image: MemoryImage(res[i].image),
+                                fit: BoxFit.cover,
+                                colorFilter: const ColorFilter.mode(
+                                    Color(0xffC19E82), BlendMode.modulate),
+                              ),
+                            ),
+                            child: Row(
+                              children: [
+                                const SizedBox(width: 15),
+                                Text(
+                                  "${res[i].time}\nSubscription",
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: Colors.white.withOpacity(0.8),
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
-                                child: Stack(
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(8),
-                                      child: ColorFiltered(
-                                        colorFilter: ColorFilter.mode(
-                                          Colors.black.withOpacity(0.5),
-                                          BlendMode.srcATop,
-                                        ),
-                                        child: Image.asset(
-                                          "${Global.wildAnimalSplashData[i].image}",
-                                          height: height * 0.6 / 4,
-                                          width: width,
-                                          fit: BoxFit.fitWidth,
-                                        ),
-                                      ),
+                                const Spacer(),
+                                SizedBox(
+                                  width: width / 3,
+                                  child: Text(
+                                    "\$ ${res[i].price}",
+                                    style: TextStyle(
+                                      fontSize: 33,
+                                      color: Colors.white.withOpacity(0.8),
+                                      fontWeight: FontWeight.w700,
                                     ),
-                                    Positioned(
-                                      top: 0,
-                                      bottom: 0,
-                                      left: 48,
-                                      right: 48,
-                                      child: Row(
-                                        children: <Widget>[
-                                          Text(
-                                            "${Global.wildAnimalSplashData[i].months}\n subscription",
-                                            style: TextStyles
-                                                .subscriptionTextStyle,
-                                          ),
-                                          Spacer(),
-                                          Text(
-                                            "${Global.wildAnimalSplashData[i].price}",
-                                            style: TextStyles
-                                                .subscriptionAmountTextStyle,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
+                                  ),
                                 ),
-                              );
-                            });
-                      } else if (snapShot.hasError) {
-                        return Center(child: Text("${snapShot.error}"));
-                      } else {
-                        return Container(
-                          color: Colors.red,
-                        );
-                      }
-                    },
+                              ],
+                            ),
+                          );
+                        },
+                        itemCount: res!.length,
+                      );
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text("${snapshot.error}"));
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(color: Colors.brown),
+                      );
+                    }
+                  },
+                  future: DBHelper.dbHelper.fetchAllRecord(
+                      tableName: "splash", data: Global.detailsOfData),
+                )),
+                SizedBox(height: 20),
+                Text(
+                  "Last Step to enjoy",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white.withOpacity(0.8),
+                    fontWeight: FontWeight.w600,
                   ),
-                ),
-                Positioned(
-                  bottom: 48,
-                  left: 20,
-                  child: Text(Strings.LAST_STEP_TO_ENJOY,
-                      style: TextStyles.buttonTextStyle),
                 ),
                 const SizedBox(height: 15),
               ],
             ),
           ),
-          Positioned(
-            bottom: -30,
-            right: -30,
+          Align(
+            alignment: Alignment.bottomRight,
             child: InkWell(
               onTap: () {
-                Navigator.of(context).pushReplacementNamed('home_Page');
+                Navigator.of(context).pushReplacementNamed("home_page");
               },
-              child: Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0xFFe9d1b5),
-                ),
-                child: Align(
-                  alignment: Alignment(-0.4, -0.4),
-                  child: Icon(
-                    Icons.arrow_forward,
-                    color: Colors.white,
-                    size: 40,
-                  ),
-                ),
-              ),
+              child: NextPage(),
             ),
-          ),
+          )
         ],
+      ),
+    );
+  }
+
+  NextPage() {
+    return Container(
+      alignment: Alignment.bottomRight,
+      height: 70,
+      width: 80,
+      padding: const EdgeInsets.only(right: 20, bottom: 15),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.5),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(100),
+        ),
+      ),
+      child: const Icon(
+        Icons.arrow_forward,
+        size: 30,
+        color: Colors.white,
       ),
     );
   }
