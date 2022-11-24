a587139827fd04bbcb264e0d33e5f7ad7175bb63
hemaxiSodvadiya <111499619+hemaxiSodvadiya@users.noreply.github.com>
2022-11-24 21:40:07 +0545

    Update text_styles.dart

--- lib/style/text_styles.dart
+++ lib/style/text_styles.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class TextStyles {
-
   TextStyles._();
 
   static final TextStyle appNameTextStyle = TextStyle(
@@ -22,37 +21,37 @@ class TextStyles {
     color: Colors.white,
     fontFamily: 'Ubuntu',
   );
-  static final TextStyle bodyTextStyle = TextStyle(
+  static final TextStyle bodyTextStyle = const TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w400,
     color: Colors.white,
     fontFamily: 'Ubuntu',
   );
-  static final TextStyle buttonTextStyle = TextStyle(
+  static final TextStyle buttonTextStyle = const TextStyle(
     fontSize: 22,
     fontWeight: FontWeight.w600,
     color: Colors.white,
     fontFamily: 'Ubuntu',
   );
-  static final TextStyle headingTextStyle = TextStyle(
+  static final TextStyle headingTextStyle = const TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.w600,
     color: Colors.white,
     fontFamily: 'Ubuntu',
   );
-  static final TextStyle subscriptionTextStyle = TextStyle(
+  static final TextStyle subscriptionTextStyle = const TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w600,
     color: Colors.white,
     fontFamily: 'Ubuntu',
   );
-  static final TextStyle subscriptionAmountTextStyle = TextStyle(
+  static final TextStyle subscriptionAmountTextStyle = const TextStyle(
     fontSize: 26,
     fontWeight: FontWeight.w600,
     color: Colors.white,
     fontFamily: 'Ubuntu',
   );
-  static final TextStyle titleTextStyle = TextStyle(
+  static final TextStyle titleTextStyle = const TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.w700,
     color: Colors.white,
