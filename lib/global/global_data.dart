d635e0cfeb219f0598af79d36370facec624b292
hemaxiSodvadiya <111499619+hemaxiSodvadiya@users.noreply.github.com>
2022-11-24 21:36:47 +0545

    Update global_data.dart

--- lib/global/global_data.dart
+++ lib/global/global_data.dart
@@ -1,47 +1,48 @@
 import 'dart:ui';
 
 import '../model/model.dart';
-class Global {
-  static Color color = const Color(0xffC19E82);
 
-  static List<AnimalData> detailsOfData = [
-    AnimalData(time: "Week", price: "1.99"),
-    AnimalData(time: "1 Month", price: "4.39"),
-    AnimalData(time: "3 Month", price: "9.99"),
-    AnimalData(time: "6 Month", price: "13"),
+class Global {
+  static List<ChooseAnimalData> detailsOfData = [
+    ChooseAnimalData(
+      time: "Week",
+      price: "1.99",
+    ),
+    ChooseAnimalData(
+      time: "1 Month",
+      price: "4.39",
+    ),
+    ChooseAnimalData(
+      time: "3 Month",
+      price: "9.99",
+    ),
+    ChooseAnimalData(
+      time: "6 Month",
+      price: "13",
+    ),
   ];
   static String category = "";
-  static List<Animal> animalData = [
-    Animal(
-      name: "Python family",
-      description: "Pythonidae",
-      category: "Snake",
-    ),
-    Animal(
-      name: "Cobras",
-      description: "Naja",
-      category: "Snake",
-    ),
-    Animal(
-      name: "German Shepherd",
-      description: "German Shepherd",
-      category: "Dog",
-    ),
-    Animal(
-      name: "Siberian Husky",
-      description: "Husky; Sibe",
-      category: "Dog",
-    ),
-    Animal(
-      name: "African elephant",
+
+  static List<CategoryAnimalData> categoryData = [
+    CategoryAnimalData(
+      name: "elephant",
       description: "Loxodonta",
       category: "Elephant",
     ),
-    Animal(
-      name: "Elephant",
-      description: "Large Rounded Ears",
-      category: "Elephant",
+    CategoryAnimalData(
+      name: "Cobras",
+      description: "cobras Snake",
+      category: "Snake",
+    ),
+    CategoryAnimalData(
+      name: "fox",
+      description: "Wild fox",
+      category: "Dog",
+    ),
+    CategoryAnimalData(
+      name: "white_fox",
+      description: "white_fox",
+      category: "Dog",
     ),
   ];
 }
-
