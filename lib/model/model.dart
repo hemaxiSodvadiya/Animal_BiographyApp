738e3b363ad8214efbe5f1dd63ec8a97d7eb57ff
hemaxiSodvadiya <111499619+hemaxiSodvadiya@users.noreply.github.com>
2022-11-24 21:32:18 +0545

    Update model.dart

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,45 +1,55 @@
 import 'dart:typed_data';
 
-class AnimalData {
+class ChooseAnimalData {
   final String time;
   final String price;
 
-  AnimalData({required this.time, required this.price});
+  ChooseAnimalData({required this.time, required this.price});
 }
 
-class DBData {
-  final String time;
-  final String price;
-  final Uint8List image;
-  DBData({required this.time, required this.price, required this.image});
-  factory DBData.fromData(e) {
-    return DBData(time: e['time'], price: e['price'], image: e['image']);
-  }
-}
 
-class Animal {
+class CategoryAnimalData {
   final String name;
   final String description;
   final String category;
 
-  Animal(
+  CategoryAnimalData(
       {required this.name, required this.category, required this.description});
 }
 
-class DBAnimal {
+
+
+
+
+
+
+
+class DBChooseAnimalData {
+  final String time;
+  final String price;
+  final Uint8List image;
+  DBChooseAnimalData(
+      {required this.time, required this.price, required this.image});
+  factory DBChooseAnimalData.fromData(e) {
+    return DBChooseAnimalData(
+        time: e['time'], price: e['price'], image: e['image']);
+  }
+}
+
+class DBCategoryAnimalData {
   final String name;
   final String description;
   final String category;
   final Uint8List image;
 
-  DBAnimal(
+  DBCategoryAnimalData(
       {required this.name,
       required this.category,
       required this.description,
       required this.image});
 
-  factory DBAnimal.fromData(Map data) {
-    return DBAnimal(
+  factory DBCategoryAnimalData.fromData(Map data) {
+    return DBCategoryAnimalData(
         name: data["name"],
         category: data["category"],
         description: data["description"],
